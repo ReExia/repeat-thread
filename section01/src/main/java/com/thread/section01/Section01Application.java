4394f420e741904aed5eac8940b7d8d2befb3c13
setsunahao <liu1023434681@qq.com>
2019-03-17 22:32:33 +0800

    add some catalogue

---
+++ section01/src/main/java/com/thread/section01/Section01Application.java
@@ -0,0 +1,13 @@
+package com.thread.section01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section01Application.class, args);
+    }
+
+}
